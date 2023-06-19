0ec760cc30178c2356f157a45f20eb16f782a59d
Rui Vieira <ruidevieira@googlemail.com>
2023-06-19 22:15:03 +0100

    Reinstate finalizer

--- controllers/finalizers.go
+++ controllers/finalizers.go
@@ -1,8 +1,18 @@
 package controllers
 
-import "context"
+import (
+	"context"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
 
 // deleteExternalDependency removes the payload processor from the ModelMesh deployment
 func (r *TrustyAIServiceReconciler) deleteExternalDependency(crName, namespace string, ctx context.Context) error {
+	// Call patchEnvVarsByLabelForDeployments with remove set to true
+	_, err := r.patchEnvVarsByLabelForDeployments(ctx, namespace, modelMeshLabelKey, modelMeshLabelValue, payloadProcessorName, crName, true)
+	if err != nil {
+		log.FromContext(ctx).Error(err, "Could not remove environment variable from ModelMesh Deployment.")
+		return err
+	}
+
 	return nil
 }
