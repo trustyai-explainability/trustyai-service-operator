8bfbaca6f8a7b4302ca6f3369d868d5f14b55125
Rui Vieira <ruidevieira@googlemail.com>
2023-11-19 20:05:24 +0000

    Add ConfigMap test for deploying with wrong keys

--- controllers/config_maps_test.go
+++ controllers/config_maps_test.go
@@ -8,6 +8,7 @@ import (
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/record"
@@ -94,6 +95,49 @@ var _ = Describe("ConfigMap tests", func() {
 				actualServiceImage, err = reconciler.getImageFromConfigMap(ctx, configMapServiceImageKey, defaultImage)
 				return err
 			}, "failed to get service image from ConfigMap")
+
+			Expect(actualOAuthImage).Should(Equal(defaultOAuthProxyImage))
+			Expect(actualServiceImage).Should(Equal(defaultImage))
+		})
+	})
+
+	Context("When deploying a ConfigMap to the operator's namespace with the wrong keys", func() {
+
+		It("Should get back the default values", func() {
+
+			serviceImage := "custom-service-image:foo"
+			oauthImage := "custom-oauth-proxy:bar"
+
+			WaitFor(func() error {
+				configMap := &corev1.ConfigMap{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      imageConfigMap,
+						Namespace: operatorNamespace,
+					},
+					Data: map[string]string{
+						"foo-oauth-image": oauthImage,
+						"foo-image":       serviceImage,
+					},
+				}
+				return k8sClient.Create(ctx, configMap)
+			}, "failed to create ConfigMap")
+
+			var actualOAuthImage string
+			var actualServiceImage string
+
+			configMapPath := operatorNamespace + "/" + imageConfigMap
+
+			Eventually(func() error {
+				var err error
+				actualOAuthImage, err = reconciler.getImageFromConfigMap(ctx, configMapOAuthProxyImageKey, defaultOAuthProxyImage)
+				return err
+			}, defaultTimeout, defaultPolling).Should(MatchError(fmt.Sprintf("configmap %s does not contain necessary keys", configMapPath)), "failed to get oauth image from ConfigMap")
+
+			Eventually(func() error {
+				var err error
+				actualServiceImage, err = reconciler.getImageFromConfigMap(ctx, configMapServiceImageKey, defaultImage)
+				return err
+			}, defaultTimeout, defaultPolling).Should(MatchError(fmt.Sprintf("configmap %s does not contain necessary keys", configMapPath)), "failed to get oauth image from ConfigMap")
 
 			Expect(actualOAuthImage).Should(Equal(defaultOAuthProxyImage))
 			Expect(actualServiceImage).Should(Equal(defaultImage))
