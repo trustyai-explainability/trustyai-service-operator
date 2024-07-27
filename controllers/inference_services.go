b341d58c3b7aba07bee6dce4ab958d0873f1ae6a
Rui Vieira <ruidevieira@googlemail.com>
2024-07-27 12:46:02 +0100

    fix: Skip InferenceService patching for KServe RawDeployment

--- controllers/inference_services.go
+++ controllers/inference_services.go
@@ -12,6 +12,12 @@ import (
 	"strings"
 )
 
+const (
+	DEPLOYMENT_MODE_MODELMESH  = "ModelMesh"
+	DEPLOYMENT_MODE_RAW        = "RawDeployment"
+	DEPLOYMENT_MODE_SERVERLESS = "Serverless"
+)
+
 func (r *TrustyAIServiceReconciler) patchEnvVarsForDeployments(ctx context.Context, instance *trustyaiopendatahubiov1alpha1.TrustyAIService, deployments []appsv1.Deployment, envVarName string, url string, remove bool) (bool, error) {
 	// Create volume and volume mount for this intance's TLS secrets
 	certVolumes := TLSCertVolumes{}
@@ -199,19 +205,25 @@ func (r *TrustyAIServiceReconciler) handleInferenceServices(ctx context.Context,
 
 	for _, infService := range inferenceServices.Items {
 		annotations := infService.GetAnnotations()
-		// Check the annotation "serving.kserve.io/deploymentMode: ModelMesh"
-		if val, ok := annotations["serving.kserve.io/deploymentMode"]; ok && val == "ModelMesh" {
-			shouldContinue, err := r.patchEnvVarsByLabelForDeployments(ctx, instance, namespace, labelKey, labelValue, envVarName, crName, remove)
-			if err != nil {
-				log.FromContext(ctx).Error(err, "Could not patch environment variables for ModelMesh deployments.")
-				return shouldContinue, err
+
+		// Check the annotation "serving.kserve.io/deploymentMode"
+		if val, ok := annotations["serving.kserve.io/deploymentMode"]; ok {
+			if val == DEPLOYMENT_MODE_RAW {
+				log.FromContext(ctx).Info("RawDeployment mode not supported by TrustyAI")
+				continue
+			} else if val == DEPLOYMENT_MODE_MODELMESH {
+				shouldContinue, err := r.patchEnvVarsByLabelForDeployments(ctx, instance, namespace, labelKey, labelValue, envVarName, crName, remove)
+				if err != nil {
+					log.FromContext(ctx).Error(err, "could not patch environment variables for ModelMesh deployments")
+					return shouldContinue, err
+				}
+				continue
 			}
-		} else {
-			err := r.patchKServe(ctx, instance, infService, namespace, crName, remove)
-			if err != nil {
-				log.FromContext(ctx).Error(err, "Could not path InferenceLogger for KServe deployment.")
-				return false, err
-			}
+		}
+		err := r.patchKServe(ctx, instance, infService, namespace, crName, remove)
+		if err != nil {
+			log.FromContext(ctx).Error(err, "could not patch InferenceLogger for KServe deployment")
+			return false, err
 		}
 	}
 	return true, nil
