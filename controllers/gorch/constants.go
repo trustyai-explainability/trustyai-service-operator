a8ced1e391abcaf2ab796e6f051d4bd8a2bb5521
Rob Geada <rob@geada.net>
2025-10-15 14:19:55 +0100

    Add servicemonitor for built-in detectors

--- controllers/gorch/constants.go
+++ controllers/gorch/constants.go
@@ -4,6 +4,7 @@ const (
 	orchestratorName     = "guardrails-orchestrator"
 	finalizerName        = "trustyai.opendatahub.io/gorch-finalizer"
 	configMapName        = "gorch-config"
+	serviceMonitorName   = "trustyai-guardrails-monitor"
 	orchestratorImageKey = "guardrails-orchestrator-image"
 	gatewayImageKey      = "guardrails-sidecar-gateway-image"
 	detectorImageKey     = "guardrails-built-in-detector-image"
