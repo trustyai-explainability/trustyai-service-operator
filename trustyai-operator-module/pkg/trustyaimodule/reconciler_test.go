cf392e9c72d80fad693275f1f36487911e35b27c
Rui Vieira <ruidevieira@googlemail.com>
2026-09-14 13:11:28 +0100

    test: align module ready condition reason

--- trustyai-operator-module/pkg/trustyaimodule/reconciler_test.go
+++ trustyai-operator-module/pkg/trustyaimodule/reconciler_test.go
@@ -120,7 +120,7 @@ var _ = Describe("TrustyAI Module Reconciler", func() {
 			readyCond := findCondition(module.Status.Conditions, string(common.ConditionTypeReady))
 			Expect(readyCond).NotTo(BeNil())
 			Expect(readyCond.Status).To(Equal(metav1.ConditionTrue))
-			Expect(readyCond.Reason).To(Equal("AllDependentsHealthy"))
+			Expect(readyCond.Reason).To(Equal("AllServicesHealthy"))
 
 			degradedCond := findCondition(module.Status.Conditions, string(common.ConditionTypeDegraded))
 			Expect(degradedCond).NotTo(BeNil())
