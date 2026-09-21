6ed41cbd537d352e729cb644e3f3c335363a16d9
Julian Payne <julpayne@redhat.com>
2026-09-21 12:45:35 +0200

    fix(evalhub): remove reference to unnecessary status-events permission in evalhub-user Role
    Signed-off-by: Julian Payne <julpayne@redhat.com>

--- controllers/evalhub/tenant_roles.go
+++ controllers/evalhub/tenant_roles.go
@@ -158,7 +158,7 @@ func tenantAdminRules(instance *evalhubv1.EvalHub) []rbacv1.PolicyRule {
 
 // tenantUserRules returns the PolicyRules for the evalhub-user Role.
 // Evaluation job lifecycle (list/create/get/update/patch/delete), read collections/providers,
-// submit via proxy, create status-events, and MLflow experiment access for submission.
+// submit via proxy and MLflow experiment access for submission.
 func tenantUserRules(instance *evalhubv1.EvalHub) []rbacv1.PolicyRule {
 	return []rbacv1.PolicyRule{
 		{
